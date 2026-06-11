8188ffcbc6362d03b4fd0cc9f7d73f2768066865
Gustav0-10 <gustavo.granado.rodrigues@escola.pr.gov.br>
2026-06-11 12:25:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("saibaMais").addEventListener("click", function () {
+
+    alert(
+        "A agricultura sustentável busca aumentar a produção de alimentos sem prejudicar o solo, a água e o meio ambiente. Práticas como rotação de culturas, controle biológico de pragas e uso consciente da água ajudam a reduzir a necessidade de produtos químicos e melhoram a qualidade das plantações."
+    );
+
+});
